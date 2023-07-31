526289fef11a5107d253f1b2910ca7793c0eb117
jonne <jonnenc@gmail.com>
2023-07-31 14:41:44 -0500

    Add basic contacts list as a sidebar. Add accompanied CSS to the contacts list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialList = [
+        {id: 0, firstName: "Jeremy", lastName: "Wang"},
+        {id: 1, firstName: "Chris", lastName: "Zhang"},
+        {id: 2, firstName: "Sam", lastName: "Chan"},
+        {id: 3, firstName: "Bob", lastName: "Williams"},
+        {id: 4, firstName: "Jeff", lastName: "Getting"}
+    ];
+    const [contacts, setContacts] = useState(initialList);
+    const [selectedContact, setSelectedContact] = useState(null);
+
+
+    return (
+        <div>
+            <h1>Contacts</h1>
+            <div
+                className = "container">
+                    <List
+                        contacts = {contacts}
+                        selectedContact = {selectedContact} //maybe not needed, keeping for now.
+                        setSelectedContact = {setSelectedContact}
+                    />
+            </div>
+        </div>
+    );
+}
+
+function List({contacts, selectedContact, setSelectedContact}) {
+    function handleSelectedContact(contactId) {
+        setSelectedContact(contactId);
+    }
+
+    return (
+        <ul
+            className = "contacts-list">
+                {contacts.map(contact => {
+                    return (
+                        <div
+                            className = "contact-item">
+                                <button
+                                    className = {`contact-button ${selectedContact === contact.id ? 'selected' : ''}`}
+                                    onClick = {() => {
+                                        handleSelectedContact(contact.id);
+                                    }}
+                                    >
+                                        {`${contact.firstName} ${contact.lastName}`}
+                                </button>
+                        </div>
+                    );
+                })}
+        </ul>
+    );
 }
 
 export default App;
