f5acbbe7cfaae9b80a77551dcb21895b1bf3fc9c
jonne <jonnenc@gmail.com>
2023-08-01 02:51:41 -0500

    Add a button that can add new contacts, with accompanied CSS. Modify CSS of the contact family to have rounded edges and a bottom-border.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,49 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
+    const [id, setId] = useState(0);
+
     const initialList = [
-        {id: 0, firstName: "Jeremy", lastName: "Wang"},
-        {id: 1, firstName: "Chris", lastName: "Zhang"},
-        {id: 2, firstName: "Sam", lastName: "Chan"},
-        {id: 3, firstName: "Bob", lastName: "Williams"},
-        {id: 4, firstName: "Jeff", lastName: "Getting"}
+        {id: -10, firstName: "Jeremy", lastName: "Wang"},
+        {id: -11, firstName: "Chris", lastName: "Zhang"},
+        {id: -12, firstName: "Sam", lastName: "Chan"},
+        {id: -13, firstName: "Bob", lastName: "Williams"},
+        {id: -14, firstName: "Jeff", lastName: "Getting"}
     ];
     const [contacts, setContacts] = useState(initialList);
     const [selectedContact, setSelectedContact] = useState(null);
 
+    function handleAddButton() {
+        setContacts([...contacts, {
+            id: id,
+            firstName: "",
+            lastName: ""
+        }])
+        setId(id + 1);
+    }
 
     return (
-        <div>
-            <h1>Contacts</h1>
-            <div
-                className = "container">
-                    <List
-                        contacts = {contacts}
-                        selectedContact = {selectedContact} //maybe not needed, keeping for now.
-                        setSelectedContact = {setSelectedContact}
-                    />
-            </div>
+        <div
+            className = "app-container">
+                <div
+                    className = "header">
+                        <h1>Contacts</h1>
+                        <button
+                            className = "add-button"
+                            onClick = {() => {
+                                handleAddButton();
+                            }}>
+                            +
+                        </button>
+                </div>
+                <div
+                    className = "container">
+                        <List
+                            contacts = {contacts}
+                            selectedContact = {selectedContact} //maybe not needed, keeping for now.
+                            setSelectedContact = {setSelectedContact}
+                        />
+                </div>
         </div>
     );
 }
@@ -38,17 +59,14 @@ function List({contacts, selectedContact, setSelectedContact}) {
             className = "contacts-list">
                 {contacts.map(contact => {
                     return (
-                        <div
-                            className = "contact-item">
-                                <button
-                                    className = {`contact-button ${selectedContact === contact.id ? 'selected' : ''}`}
-                                    onClick = {() => {
-                                        handleSelectedContact(contact.id);
-                                    }}
-                                    >
-                                        {`${contact.firstName} ${contact.lastName}`}
-                                </button>
-                        </div>
+                        <button
+                            className = {`contact-button ${selectedContact === contact.id ? 'selected' : ''}`}
+                            onClick = {() => {
+                                handleSelectedContact(contact.id);
+                            }}
+                            >
+                                {`${contact.firstName} ${contact.lastName}`}
+                        </button>
                     );
                 })}
         </ul>
